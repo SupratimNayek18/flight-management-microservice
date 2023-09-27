e8fe075d9a2522f7ac5c59f67987744b1b04a7e1
Nayek <supratim.nayek@capgemini.com>
2023-09-27 15:52:00 +0530

    Changes in home component

--- flight-management-frontend/src/app/app.module.ts
+++ flight-management-frontend/src/app/app.module.ts
@@ -9,6 +9,8 @@ import { HttpClientModule } from '@angular/common/http';
 import { SigninComponent } from './signin/signin.component';
 import { SearchFlightsComponent } from './search-flights/search-flights.component';
 
+
+
 @NgModule({
   declarations: [AppComponent, RegisterComponent, HomeComponent, SigninComponent, SearchFlightsComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
