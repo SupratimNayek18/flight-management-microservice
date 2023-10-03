313fbee344d9830ea1f70ebbaea772e69c6d6951
Potti <potti-varun.kumar@capgemini.com>
2023-10-03 15:32:38 +0530

    Added code readablity

--- booking-microservice/src/test/java/com/spring/bookingmicroservice/BookingRepositoryTests.java
+++ booking-microservice/src/test/java/com/spring/bookingmicroservice/BookingRepositoryTests.java
@@ -28,7 +28,7 @@ public class BookingRepositoryTests {
     @Test
     public void givenBookingShouldReturnBookingObject() {
         // Given
-        Booking booking1 = new Booking("1", 123, "User1", true, false, null);
+        Booking booking1 = new Booking("1", 123, "User1", true, false, null, null);
 
         // When
         bookingRepository.save(booking1); // Data is saved into the Database
@@ -39,26 +39,11 @@ public class BookingRepositoryTests {
         assertEquals(booking1.getUserName(), booking2.getUserName());
     }
 
-    @Test
-    public void getAllMustReturnAllBookings() {
-        // Given
-        Booking booking3 = new Booking("2", 456, "User2", true, false, null);
-        Booking booking4 = new Booking("3", 789, "User3", true, false, null);
-        bookingRepository.save(booking3); // Save the Data in the Database
-        bookingRepository.save(booking4); // Save the Data in the Database
-
-        // When
-        List<Booking> bookingList = bookingRepository.findAll();
-
-        // Then
-        assertEquals(4, bookingList.size());
-        assertEquals("User3", bookingList.get(3).getUserName());
-    }
 
     @Test
     public void givenBookingIdShouldReturnBookingObject() {
         // Given
-        Booking booking1 = new Booking("1", 123, "User1", true, false, null);
+        Booking booking1 = new Booking("1", 123, "User1", true, false, null, null);
 
         // When
         bookingRepository.save(booking1); // Data is saved into the Database
@@ -82,7 +67,7 @@ public class BookingRepositoryTests {
     @Test
     public void givenUserNameShouldReturnBookingList() {
         // Given
-        Booking booking1 = new Booking("1", 123, "User1", true, false, null);
+        Booking booking1 = new Booking("1", 123, "User1", true, false, null, null);
         bookingRepository.save(booking1);
 
         // When
